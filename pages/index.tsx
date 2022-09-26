f1ee3ec245729e33835dc95b8dc59f4b698b70d1
Behrooz Arabzade <behrooz.arabzade@gmail.com>
2022-09-26 11:11:47 +0330

    Some changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,36 +3,35 @@ import type { NextPage } from "next";
 // Components
 import Login from "components/molecules/Login/Login";
 
-
-
 //Icons
 import Head from "next/head";
 import Image from "next/image";
-
-
-
-
 
 //Css(Style Sheet)
 import styles from "../styles/Home.module.css";
 import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
 
-
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Container>
-        <Grid display="flex" sx={{height:"100vh"}} justifyContent="center" alignItems="center" mt={3}>
-          <Login onSubmitClicked={(data) => {
-            console.log("user login", data)
-          }}
-            onRegisterClicked={() => {
-              console.log("register")
+        <Grid
+          display="flex"
+          sx={{ height: "100vh" }}
+          justifyContent="center"
+          alignItems="center"
+          mt={3}
+        >
+          <Login
+            onSubmitClicked={(data) => {
+              console.log("user login", data);
             }}
-            onForgetPasswordClicked={()=> {
-              console.log("forgetPassword")
+            onRegisterClicked={() => {
+              console.log("register");
+            }}
+            onForgetPasswordClicked={() => {
+              console.log("forgetPassword");
             }}
           />
         </Grid>
