c4861e34e114bd52b0f7f5fcecfa288a41f2afbb
Behrooz Arabzade <behrooz.arabzade@gmail.com>
2022-09-29 21:55:35 +0330

    Minor changes

--- components/molecules/ReactionButton/ReactionButton.tsx
+++ components/molecules/ReactionButton/ReactionButton.tsx
@@ -1,103 +1,90 @@
-import React, { useState, MouseEvent, ReactNode, FC } from 'react'
-import { Button, Popover } from '@mui/material';
-
+import React, { useState, MouseEvent, ReactNode, FC } from "react";
+import { Button, Popover } from "@mui/material";
 
 //Icon
-import Like from 'components/molecules/ReactionButton/Like';
-
+import Like from "components/molecules/ReactionButton/Like";
 
 //Css(Style Sheet)
-import useReactionButton from './useStyle';
-
+import useReactionButton from "./useStyle";
 
 //InterFace
 interface IReactionButton {
-    items: Array<{
-        id: number;
-        icon: ReactNode;
-    }>;
-    onReactionSelecte: (reactionId: number) => void;
-    selectedItemId: number;
+  items: Array<{
+    id: number;
+    icon: ReactNode;
+  }>;
+  onReactionSelecte: (reactionId: number) => void;
+  selectedItemId: number;
 }
-
-
-
 
 const ReactionButton: FC<IReactionButton> = ({
-    items,
-    onReactionSelecte,
-    selectedItemId
+  items,
+  onReactionSelecte,
+  selectedItemId,
 }) => {
+  const { classes, cx } = useReactionButton();
 
-    const { classes, cx } = useReactionButton();
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
-    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [openReaction, setOpenReaction] = useState<boolean>(false);
 
-    const [openReaction, setOpenReaction] = useState<boolean>(false);
+  const clickHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    setOpenReaction(true);
+  };
 
-    const [select, setSelect] = useState<number>(0);
+  const selectHandler = (id: number) => () => {
+    onReactionSelecte(id);
+  };
 
-    const clickHandler = (event: MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-        setOpenReaction(true);
+  const closeHandler = () => {
+    setAnchorEl(null);
+    setOpenReaction(false);
+  };
 
-    };
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
-    const selectHandler = (id: number) => () => {
-
-        onReactionSelecte(id)
-
-
-
-    };
-
-    const closeHandler = () => {
-        setAnchorEl(null);
-        setOpenReaction(false);
-    };
-
-    const open = Boolean(anchorEl);
-    const id = open
-        ? 'simple-popover'
-        : undefined;
-
-
-    return (
-        <div>
-            <Button color={openReaction ? "warning" : "inherit"} className={cx(classes.root)} aria-describedby={id} onClick={clickHandler}>
-                <Like />
-            </Button>
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={closeHandler}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-            >
-                <div className={cx(classes.listIcons)} >
-                    {
-                        items.map(item => {
-                            return (
-                                <Button key={item.id}
-                                    onClick={selectHandler(item.id)}
-                                    color={selectedItemId === item.id ? "warning" : "inherit"}>
-                                    {item.icon}
-                                </Button>
-                            )
-                        }
-                        )
-                    }
-                </div>
-            </Popover >
-        </div >
-    )
-}
+  return (
+    <>
+      <Button
+        color={openReaction ? "warning" : "inherit"}
+        className={cx(classes.root)}
+        aria-describedby={id}
+        onClick={clickHandler}
+      >
+        <Like />
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={closeHandler}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+      >
+        <div className={cx(classes.listIcons)}>
+          {items.map((item) => {
+            return (
+              <Button
+                key={item.id}
+                onClick={selectHandler(item.id)}
+                color={selectedItemId === item.id ? "warning" : "inherit"}
+              >
+                {item.icon}
+              </Button>
+            );
+          })}
+        </div>
+      </Popover>
+    </>
+  );
+};
 
 export default ReactionButton;
