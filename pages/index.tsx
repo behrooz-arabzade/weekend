ceac82ab48328564c348cda0ea0c9db2243f0aa0
Behrooz Arabzade <behrooz.arabzade@gmail.com>
2022-10-02 09:29:49 +0330

    Done without api call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,17 @@
 import type { NextPage } from "next";
-
-// Components
-import MainNavigation from "components/molecules/MainNavigation/MainNavigation";
-import HomeIcon from "components/molecules/MainNavigation/HomeIcon";
-import SearchIcon from "components/molecules/MainNavigation/Search";
-import Create from "components/molecules/MainNavigation/Create";
-import EventActive from "./../components/molecules/MainNavigation/EventActive";
-import MessageIcon from "./../components/molecules/MainNavigation/MessageIcon";
-
-
-
-import FeedPostFooter from "components/molecules/FeedPostFooter/FeedPostFooter";
-import FeedPostHeader from './../components/molecules/FeedPostHeader/FeedPostHeader';
-import ReactionButton from "components/molecules/ReactionButton/ReactionButton";
-import Like from "components/molecules/ReactionButton/Like";
-import UnLike from "components/molecules/ReactionButton/UnLike";
-import Idea from "components/molecules/ReactionButton/Idea";
-import FriendCard from "components/molecules/FriendCard/FriendCard";
-
-
-
-//Icons
-import Head from "next/head";
-import Image from "next/image";
-
-//Css(Style Sheet)
-import styles from "../styles/Home.module.css";
-import { Container } from "@mui/system";
-import { Grid } from "@mui/material";
-import FeedPostComment from "components/molecules/FeedPostComment/FeedPostComment";
-
-
-
-
-
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const authorized = Boolean(localStorage.getItem("token"))
+  const router = useRouter();
 
+  if (!authorized) {
+    router.push("/login")
+    return null
+  }
 
-  return (
-    <div className={styles.container}>
-      <Container>
-        <Grid height="100vh"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <FeedPostComment
-            userPicSrc="Images/Aryan.jpg"
-            userName="Aryan_Salemabadi"
-            comment=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi"
-            likeCount={10}
-            onHideClicked={() => {
-              console.log("comment hide")
-            }}
-            onReportClicked={() => {
-              console.log("reportcomment")
-            }}
-          />
-        </Grid>
-      </Container>
-    </div>
-  );
+  router.push("/feed")
+  return null;
 };
+
 export default Home;
