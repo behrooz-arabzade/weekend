fb9f06548f26dc3f3519b0f2eba6eb1b51f323e1
Behrooz Arabzade <behrooz.arabzade@gmail.com>
2022-10-06 00:04:40 +0330

    Added token to Authorization header

--- apis/Api.ts
+++ apis/Api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 class ApiClass {
   public axiosInstance: AxiosInstance;
@@ -12,7 +12,17 @@ class ApiClass {
         Accept: "application/json",
       },
     });
+
+    this.axiosInstance.interceptors.request.use(this.onFulfilled);
   }
+
+  onFulfilled = (config: AxiosRequestConfig): AxiosRequestConfig => {
+    config.headers!["Authorization"] = `Bearer ${localStorage.getItem(
+      "token"
+    )}`;
+
+    return config;
+  };
 }
 
 const Api = new ApiClass("http://test.ir");
