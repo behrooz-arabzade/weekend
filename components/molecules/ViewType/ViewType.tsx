d5b6f1c6941279eaa180d55961a3eb5a2cef1d4f
Behrooz Arabzade <behrooz.arabzade@gmail.com>
2022-10-05 18:16:23 +0330

    Minor changes

--- components/molecules/ViewType/ViewType.tsx
+++ components/molecules/ViewType/ViewType.tsx
@@ -1,114 +1,116 @@
-import React,{useState, useRef, ReactNode, FC} from 'react';
-import { Card, Paper, ClickAwayListener, MenuList, MenuItem, Popper, Button, ButtonGroup, Grow } from '@mui/material';
-
-
+import React, { useState, useRef, ReactNode, FC } from "react";
+import {
+  Card,
+  Paper,
+  ClickAwayListener,
+  MenuList,
+  MenuItem,
+  Popper,
+  Button,
+  ButtonGroup,
+  Grow,
+} from "@mui/material";
 
 //icon
-import WindowOutlinedIcon from '@mui/icons-material/WindowOutlined';
 
-
-
-
-
+interface IType {
+  name: string;
+  icon: ReactNode;
+}
 //InterFace
 interface IViewType {
-    items:Array<{
-        name:string;
-        icon:ReactNode;
-    }>;
-    defaultType:number;
-    onViewTypeClicked:(viewName: string) => void;
+  items: Array<IType>;
+  selectedTypeName: string;
+  onViewTypeClicked: (viewName: string) => void;
 }
 
-const ViewType:FC<IViewType> = ({
-    items,
-    defaultType,
-    onViewTypeClicked
-
+const ViewType: FC<IViewType> = ({
+  items,
+  selectedTypeName,
+  onViewTypeClicked,
 }) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
 
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    index: number
+  ) => {
+    onViewTypeClicked(items[index].name);
+    setOpen(false);
+  };
 
-    const [open, setOpen] = useState<boolean>(false);
-    const anchorRef = useRef<HTMLDivElement>(null);
-    const [selectedIndex, setSelectedIndex] = useState<number>(defaultType);
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
 
-    const handleMenuItemClick = (
-        event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-        index: number,
-    ) => {
-        setSelectedIndex(index);
-        setOpen(false);
-    };
+  const handleClose = (event: Event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
 
-    const handleToggle = () => {
-        setOpen((prevOpen) => !prevOpen);
-        onViewTypeClicked(items[selectedIndex].name)
-        
-    };
-
-    const handleClose = (event: Event) => {
-        if (
-            anchorRef.current &&
-            anchorRef.current.contains(event.target as HTMLElement)
-        ) {
-            return;
-        }
-
-        setOpen(false);
-    };
-    return (
-        <Card>
-            <ButtonGroup  variant="contained" ref={anchorRef} aria-label="split button">
-                <Button
-                    size="small"
-                    aria-controls={open ? 'split-button-menu' : undefined}
-                    aria-expanded={open ? 'true' : undefined}
-                    aria-label="select merge strategy"
-                    aria-haspopup="menu"
-                    onClick={handleToggle}
-                    color="inherit"
-                >
-                    {items[selectedIndex].icon}
-                </Button>
-            </ButtonGroup>
-            <Popper
-                sx={{
-                    zIndex: 1,
-                }}
-                open={open}
-                anchorEl={anchorRef.current}
-                role={undefined}
-                transition
-                disablePortal
-            >
-                {({ TransitionProps, placement }) => (
-                    <Grow
-                        {...TransitionProps}
-                        style={{
-                            transformOrigin:
-                                placement === 'bottom' ? 'center top' : 'center bottom',
-                        }}
+    setOpen(false);
+  };
+  return (
+    <Card>
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="split button"
+      >
+        <Button
+          size="small"
+          aria-controls={open ? "split-button-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+          color="inherit"
+        >
+          {items.find((i) => i.name === selectedTypeName)?.icon}
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {items.map((item, index) => (
+                    <MenuItem
+                      key={item.name}
+                      selected={item.name === selectedTypeName}
+                      onClick={(event) => handleMenuItemClick(event, index)}
                     >
-                        <Paper>
-                            <ClickAwayListener onClickAway={handleClose}>
-                                <MenuList id="split-button-menu" autoFocusItem>
-                                    {items.map((item, index) => (
-                                        <MenuItem
-                                            key={index}
-                                            selected={index === selectedIndex}
-                                            onClick={ event => handleMenuItemClick(event, index)}
-                                        >
-                                            {item.icon}
-                                        </MenuItem>
-                                    ))}
-                                </MenuList>
-                            </ClickAwayListener>
-                        </Paper>
-                    </Grow>
-                )}
-            </Popper>
-        </Card >
-    )
-}
+                      {item.icon}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </Card>
+  );
+};
 
 export default ViewType;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,17 @@
 import type { NextPage } from "next";
 
 // Components
-import MainNavigation from "components/molecules/MainNavigation/MainNavigation";
-import HomeIcon from "components/molecules/MainNavigation/HomeIcon";
-import SearchIcon from "components/molecules/MainNavigation/Search";
-import Create from "components/molecules/MainNavigation/Create";
-import EventActive from "./../components/molecules/MainNavigation/EventActive";
-import MessageIcon from "./../components/molecules/MainNavigation/MessageIcon";
-import FeedPostFooter from "components/molecules/FeedPostFooter/FeedPostFooter";
-import FeedPostHeader from './../components/molecules/FeedPostHeader/FeedPostHeader';
-import ReactionButton from "components/molecules/ReactionButton/ReactionButton";
-import Like from "components/molecules/ReactionButton/Like";
-import UnLike from "components/molecules/ReactionButton/UnLike";
-import Idea from "components/molecules/ReactionButton/Idea";
 import ViewType from "components/molecules/ViewType/ViewType";
-import GridIcon from 'components/molecules/ViewType/GridIcon';
-import ListViewWithDetail from "components/molecules/ViewType/ListviewWithDetail";
-import ListViewWithoutDetail from 'components/molecules/ViewType/ListViewWithoutDetail';
+import GridIcon from "components/molecules/ViewType/GridIcon";
+import ListViewWithDetail from "components/molecules/ViewType/ListViewWithDetail";
+import ListViewWithoutDetail from "components/molecules/ViewType/ListViewWithoutDetail";
 
 //Icons
-import Head from "next/head";
-import Image from "next/image";
 
 //Css(Style Sheet)
 import styles from "../styles/Home.module.css";
 import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
-
-
-
-
 
 const Home: NextPage = () => {
   return (
@@ -42,27 +24,23 @@ const Home: NextPage = () => {
           alignItems="center"
         >
           <ViewType
-            items={
-              [
-                {
-                  name: "grid",
-                  icon: <GridIcon />,
-                },
-                {
-                  name: "listview with detail",
-                  icon: <ListViewWithDetail />,
-                },
-                {
-                  name: "Listview without detail",
-                  icon: <ListViewWithoutDetail />
-                }
-              ]
-            }
-
-            defaultType={0}
-
-            onViewTypeClicked={(viewName)=>{
-              console.log("click to" , viewName)
+            items={[
+              {
+                name: "grid",
+                icon: <GridIcon />,
+              },
+              {
+                name: "listview with detail",
+                icon: <ListViewWithDetail />,
+              },
+              {
+                name: "listview without detail",
+                icon: <ListViewWithoutDetail />,
+              },
+            ]}
+            selectedTypeName="grid"
+            onViewTypeClicked={(viewName) => {
+              console.log("click to", viewName);
             }}
           />
         </Grid>
