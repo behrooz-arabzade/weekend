3cabdb6161ae79805ba04522eb99f12df094c84e
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2022-10-05 16:32:17 +0330

    create viewtype on component-viewtype branch

---
+++ components/molecules/ViewType/GridIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//Icon
+import WindowOutlinedIcon from '@mui/icons-material/WindowOutlined';
+
+
+const GridIcon = () => {
+  return (
+        <WindowOutlinedIcon></WindowOutlinedIcon>
+  )
+}
+
+export default GridIcon;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,10 @@ import ReactionButton from "components/molecules/ReactionButton/ReactionButton";
 import Like from "components/molecules/ReactionButton/Like";
 import UnLike from "components/molecules/ReactionButton/UnLike";
 import Idea from "components/molecules/ReactionButton/Idea";
-
-
+import ViewType from "components/molecules/ViewType/ViewType";
+import GridIcon from 'components/molecules/ViewType/GridIcon';
+import ListViewWithDetail from "components/molecules/ViewType/ListviewWithDetail";
+import ListViewWithoutDetail from 'components/molecules/ViewType/ListViewWithoutDetail';
 
 //Icons
 import Head from "next/head";
@@ -25,29 +27,46 @@ import styles from "../styles/Home.module.css";
 import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
 
+
+
+
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Container>
-    <Grid 
-    height="100vh"
-     display="flex"
-      justifyContent="center"
-       alignItems="center"
-       >
-        <FeedPostFooter 
-          reactionCount={10}
-          currentReactionId = {3}
-          onSaveClicked={()=>{
-            console.log("post saved")
-          }}
-          onCommentsClicked={()=>{
-            console.log("comment")
-          }}
+        <Grid
+          height="100vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <ViewType
+            items={
+              [
+                {
+                  name: "grid",
+                  icon: <GridIcon />,
+                },
+                {
+                  name: "listview with detail",
+                  icon: <ListViewWithDetail />,
+                },
+                {
+                  name: "Listview without detail",
+                  icon: <ListViewWithoutDetail />
+                }
+              ]
+            }
 
-        />
-    </Grid>
-    </Container>
+            defaultType={0}
+
+            onViewTypeClicked={(viewName)=>{
+              console.log("click to" , viewName)
+            }}
+          />
+        </Grid>
+      </Container>
     </div>
   );
 };
