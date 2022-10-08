933efa7c0c6589f07e145c36e8d6e09a135b8cb1
Behrooz Arabzade <behrooz.arabzade@gmail.com>
2022-10-09 00:07:07 +0330

    Minor changes, need refactoring

--- components/molecules/EventDetails/useStyle.ts
+++ components/molecules/EventDetails/useStyle.ts
@@ -1,15 +1,10 @@
 import { makeStyles } from "tss-react/mui";
 
 const useEventDetails = makeStyles()((theme) => ({
-
-    member: {
-
-        margin: "20px 0px",
-        padding: "10px",
-        width: "300px"
-
-    }
-
-
+  member: {
+    margin: "20px 0px",
+    padding: "10px",
+    width: "300px",
+  },
 }));
 export default useEventDetails;
